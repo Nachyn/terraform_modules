46bae99221ad74609feedb8861d4d5c4d220d8bd
Nachyn <nachyn@outlook.com>
2025-10-25 15:30:59 +0700

    modec to live folder

--- live/prod/services/webserver-cluster/main.tf
+++ live/prod/services/webserver-cluster/main.tf
@@ -3,26 +3,29 @@ provider "aws" {
 }
 
 module "webserver-cluster" {
-  source = "../../../modules/services/webserver-cluster"
+  source = "../../../../modules/services/webserver-cluster"
 
-  cluster_name           = "webservers-stage"
+  cluster_name           = "webservers-prod"
   db_remote_state_bucket = "asan-li-terraform-state"
   db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
 
-  ami           = "ami-0fb653ca2d3203ac1"
   instance_type = "t3.micro"
-  server_text = "New server text || 123456"
+  min_size      = 1
+  max_size      = 5
 
-  min_size           = 1
-  max_size           = 5
-  enable_autoscaling = false
+  custom_tags = {
+    Owner     = "team-foo"
+    ManagedBy = "Terraform"
+  }
+
+  enable_autoscaling = true
 }
 
 terraform {
   backend "s3" {
     # Укажите здесь имя своей корзины!
     bucket = "asan-li-terraform-state"
-    key    = "stage/services/webserver-cluster/terraform.tfstate"
+    key    = "prod/services/webserver-cluster/terraform.tfstate"
     region = "us-east-2"
     # Укажите здесь имя своей таблицы DynamoDB!
     dynamodb_table = "terraform-locks"
--- live/stage/services/webserver-cluster/main.tf
+++ live/stage/services/webserver-cluster/main.tf
@@ -3,29 +3,26 @@ provider "aws" {
 }
 
 module "webserver-cluster" {
-  source = "../../../modules/services/webserver-cluster"
+  source = "../../../../modules/services/webserver-cluster"
 
-  cluster_name           = "webservers-prod"
+  cluster_name           = "webservers-stage"
   db_remote_state_bucket = "asan-li-terraform-state"
   db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
 
+  ami           = "ami-0fb653ca2d3203ac1"
   instance_type = "t3.micro"
-  min_size      = 1
-  max_size      = 5
+  server_text = "New server text || 123456"
 
-  custom_tags = {
-    Owner     = "team-foo"
-    ManagedBy = "Terraform"
-  }
-
-  enable_autoscaling = true
+  min_size           = 1
+  max_size           = 5
+  enable_autoscaling = false
 }
 
 terraform {
   backend "s3" {
     # Укажите здесь имя своей корзины!
     bucket = "asan-li-terraform-state"
-    key    = "prod/services/webserver-cluster/terraform.tfstate"
+    key    = "stage/services/webserver-cluster/terraform.tfstate"
     region = "us-east-2"
     # Укажите здесь имя своей таблицы DynamoDB!
     dynamodb_table = "terraform-locks"
